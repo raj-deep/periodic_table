85567168532d5b61ca8607e691f1c9106b2293b6
raj-deep <deepraj181200@gmail.com>
2024-09-29 09:00:34 +0530

    third commit: tailwind integrated

---
+++ src/Square.tsx
@@ -0,0 +1,11 @@
+import './Square.css'
+
+function Square(){
+    return(
+        <button className="bg-blue-500 hover:bg-blue-700">
+            
+        </button>
+    )
+}
+
+export default Square
