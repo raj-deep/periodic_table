02d497b6dc25478cea0062bf478c499c8c03dcc7
raj-deep <deepraj181200@gmail.com>
2024-10-10 14:51:17 +0530

    minor CSS designing

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,6 @@ import "./App.css";
 
 const App: React.FC = () => {
   const [lightOn, setLightOn] = useState(false);
-  // Example data for rows
-  // const periodicTableData = [
-  //   [
-  //     { symbol: "H", name: "Hydrogen", number: 1 },
-  //     { symbol: "He", name: "Helium", number: 2 },
-  //   ],
-  //   [
-  //     { symbol: "Li", name: "Lithium", number: 3 },
-  //     { symbol: "Be", name: "Beryllium", number: 4 },
-  //     { symbol: "B", name: "Boron", number: 5 },
-  //     { symbol: "C", name: "Carbon", number: 6 },
-  //   ],
-  //   // More rows...
-  // ];
 
   const handleClick = () => {
     setLightOn(!lightOn);
@@ -27,170 +13,169 @@ const App: React.FC = () => {
 
   return (
     <>
-      <nav className="h-20 border-red-400 border-2 border-solid flex flex-col justify-center items-center">
-        <button
-          className="lightOn text-white border border-red-400 border-solid absolute top-50 left-0 hover:pointer"
-          onClick={handleClick}
-        >
-          Lights On
-        </button>
-        <div className="text-white">Periodic Table</div>
-        <div className="text-white">Move your mouse and find the elements</div>
-      </nav>
-      <div className="container mx-auto">
-        <div id="row1" className="flex justify-between">
-          <Square
-            className="nonMetals"
-            symbol="H"
-            number={1}
-            name="Hydrogen"
-            isPressed={lightOn}
-          />
-          <Square
-            className="absolute right-0 nobleGases"
-            symbol="He"
-            number={2}
-            name="Helium"
-            isPressed={lightOn}
-          />
-        </div>
-        <div id="row2" className="flex justify-between">
-          <div className="component1 flex space-x-1 mt-1">
-            <Square
-              className="AlMetals"
-              symbol="Li"
-              number={3}
-              name="Lithium"
-              isPressed={lightOn}
-            />
-            <Square
-              className="AlEarthMetals"
-              symbol="Be"
-              number={4}
-              name="Beryllium"
-              isPressed={lightOn}
-            />
+      <div>
+        <div className="background-layer"></div>
+        <nav className="h-20 mt-2 flex flex-col justify-center items-center">
+          <div className="absolute left-5">
+            <label className="switch">
+              <input type="checkbox" onChange={handleClick} />
+              <span className="slider"></span>
+            </label>
           </div>
-          <div className="component2 flex space-x-1 mt-1">
-            <Square
-              className="metalloid"
-              symbol="B"
-              number={5}
-              name="Boron"
-              isPressed={lightOn}
-            />
-            <Square
-              className="nonMetal"
-              symbol="C"
-              number={6}
-              name="Carbon"
-              isPressed={lightOn}
-            />
-            <Square
-              className="nonMetal"
-              symbol="N"
-              number={7}
-              name="Nitrogen"
-              isPressed={lightOn}
-            />
-            <Square
-              className="nonMetal"
-              symbol="O"
-              number={8}
-              name="Oxygen"
-              isPressed={lightOn}
-            />
-            <Square
-              className="nonMetal"
-              symbol="F"
-              number={9}
-              name="Fluorine"
-              isPressed={lightOn}
-            />
-            <Square
-              className="nobleGases"
-              symbol="Ne"
-              number={10}
-              name="Neon"
-              isPressed={lightOn}
-            />
-          </div>
-        </div>
 
-        <div id="row3" className="flex justify-between">
-          <div className="component1 flex space-x-1 mt-1">
+          <div className="text-white text-4xl">THE PERIODIC TABLE</div>
+          <div className="text-white">Move your mouse to find the elements</div>
+        </nav>
+        <div className="container mt-5 ml-5">
+          <div id="row1" className="flex justify-between">
             <Square
-              className="AlMetals"
-              symbol="Na"
-              number={11}
-              name="Sodium"
+              className="nonMetals"
+              symbol="H"
+              number={1}
+              name="Hydrogen"
               isPressed={lightOn}
             />
             <Square
-              className="AlEarthMetals"
-              symbol="Mg"
-              number={12}
-              name="Magnesium"
-              isPressed={lightOn}
-            />
-          </div>
-          <div className="component2 flex space-x-1 mt-1">
-            <Square
-              className="metalloid"
-              symbol="Al"
-              number={13}
-              name="Aluminum"
-              isPressed={lightOn}
-            />
-            <Square
-              className="nonMetal"
-              symbol="Si"
-              number={14}
-              name="Silicon"
-              isPressed={lightOn}
-            />
-            <Square
-              className="nonMetal"
-              symbol="P"
-              number={15}
-              name="Phosphorus"
-              isPressed={lightOn}
-            />
-            <Square
-              className="nonMetal"
-              symbol="S"
-              number={16}
-              name="Sulfur"
-              isPressed={lightOn}
-            />
-            <Square
-              className="halogen"
-              symbol="Cl"
-              number={17}
-              name="Chlorine"
-              isPressed={lightOn}
-            />
-            <Square
-              className="nobleGases"
-              symbol="Ar"
-              number={18}
-              name="Argon"
+              className="absolute right-0 nobleGases"
+              symbol="He"
+              number={2}
+              name="Helium"
               isPressed={lightOn}
             />
           </div>
-        </div>
-        <div className="periodic-table">
-          {periodicTableData.map((row, index) => (
-            <Row key={index} elements={row} isPressed={lightOn} />
-          ))}
+          <div id="row2" className="flex justify-between">
+            <div className="component1 flex space-x mt-1">
+              <Square
+                className="AlMetals"
+                symbol="Li"
+                number={3}
+                name="Lithium"
+                isPressed={lightOn}
+              />
+              <Square
+                className="AlEarthMetals"
+                symbol="Be"
+                number={4}
+                name="Beryllium"
+                isPressed={lightOn}
+              />
+            </div>
+            <div className="component2 flex space-x mt-1">
+              <Square
+                className="metalloid"
+                symbol="B"
+                number={5}
+                name="Boron"
+                isPressed={lightOn}
+              />
+              <Square
+                className="nonMetal"
+                symbol="C"
+                number={6}
+                name="Carbon"
+                isPressed={lightOn}
+              />
+              <Square
+                className="nonMetal"
+                symbol="N"
+                number={7}
+                name="Nitrogen"
+                isPressed={lightOn}
+              />
+              <Square
+                className="nonMetal"
+                symbol="O"
+                number={8}
+                name="Oxygen"
+                isPressed={lightOn}
+              />
+              <Square
+                className="nonMetal"
+                symbol="F"
+                number={9}
+                name="Fluorine"
+                isPressed={lightOn}
+              />
+              <Square
+                className="nobleGases"
+                symbol="Ne"
+                number={10}
+                name="Neon"
+                isPressed={lightOn}
+              />
+            </div>
+          </div>
+
+          <div id="row3" className="flex justify-between">
+            <div className="component1 flex space-x mt-1">
+              <Square
+                className="AlMetals"
+                symbol="Na"
+                number={11}
+                name="Sodium"
+                isPressed={lightOn}
+              />
+              <Square
+                className="AlEarthMetals"
+                symbol="Mg"
+                number={12}
+                name="Magnesium"
+                isPressed={lightOn}
+              />
+            </div>
+            <div className="component2 flex space-x mt-1">
+              <Square
+                className="metalloid"
+                symbol="Al"
+                number={13}
+                name="Aluminum"
+                isPressed={lightOn}
+              />
+              <Square
+                className="nonMetal"
+                symbol="Si"
+                number={14}
+                name="Silicon"
+                isPressed={lightOn}
+              />
+              <Square
+                className="nonMetal"
+                symbol="P"
+                number={15}
+                name="Phosphorus"
+                isPressed={lightOn}
+              />
+              <Square
+                className="nonMetal"
+                symbol="S"
+                number={16}
+                name="Sulfur"
+                isPressed={lightOn}
+              />
+              <Square
+                className="halogen"
+                symbol="Cl"
+                number={17}
+                name="Chlorine"
+                isPressed={lightOn}
+              />
+              <Square
+                className="nobleGases"
+                symbol="Ar"
+                number={18}
+                name="Argon"
+                isPressed={lightOn}
+              />
+            </div>
+          </div>
+          <div className="periodic-table">
+            {periodicTableData.map((row, index) => (
+              <Row key={index} elements={row} isPressed={lightOn} />
+            ))}
+          </div>
         </div>
       </div>
     </>
-    // <div style={styles.container}>
-    //   {periodicTableData.map((row, index) => (
-    //     <Row key={index} elements={row} />
-    //   ))}
-    // </div>
   );
 };
 
--- src/Square.tsx
+++ src/Square.tsx
@@ -18,14 +18,19 @@ const Square: React.FC<SquareProps> = ({
 }) => {
   return (
     <button
-      className={`inline-block w-20 flex-row text-xs p-2 relative ${className} ${
+      className={`inline-block flex-row text-xs p-1 pb-0 relative rounded ${className} ${
         isPressed ? "blinking" : "square"
       }`}
       // style={{ flex: "1 1 5%" }}
     >
       <div className="absolute top-2 left-2 text-xs position-0">{number}</div>
-      <div className="font-bold text-xl">{symbol}</div>
-      <div className="text-xs">{name}</div>
+      <div className="font-bold m-1 text-2xl">{symbol}</div>
+      <div
+        className="text-xs flex-wrap break-words pb-1 pr-2"
+        style={{ width: "calc(84vw/18)" }}
+      >
+        {name}
+      </div>
     </button>
   );
 };
